58cba825f77b1dbbe2d27c0e127639cc0ec8e228
PriseRed <webdaemonick@gmail.com>
2022-03-25 15:42:59 +1000

    Прописаны расы в data.js

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,23 @@
 $(document).ready(function () {    
     console.log(data);
+	var races = "";
+	for (var i = 0; i < data.races.length; i++) {
+		races += "<button class='get_race' data-id='" + i + "'>" + data.races[i].title + "</button>";
+	}
+	$(".Operative_Choose").append(races);
 	$("body").overlayScrollbars({ });
+});
+
+$("body").on("click", ".get_race", function(){
+	var id = $(this).data("id");
+	$(".get_race").removeClass("active");
+	$(this).addClass("active");
+	$("#RaceH3").html(data.races[id].title);
+	$("#RaceDesc").html(data.races[id].description);
+});
+
+$(".BtnTo").on("click", function(){
+	$(".DialogBlock").removeClass("active");
+	console.log($(this).data("to"));
+	$(".DialogBlock[data-block='" + $(this).data("to") + "']").addClass("active");
 });
